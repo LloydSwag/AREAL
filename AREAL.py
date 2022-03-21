35061625396da6de36284d4931dff893d2c60ec1
porder11 <93432262+porder11@users.noreply.github.com>
2022-03-21 15:08:10 +0100

    forbedret rektangel og trekant funksjon.

--- AREAL.py
+++ AREAL.py
@@ -1,9 +1,7 @@
 import os
-
 import math as m
-# Funksjon for å tømme terminalen
-=======
 
+# Funksjon for å tømme terminalen
 def clear():
     os.system('cls')
 
@@ -17,45 +15,35 @@ def skriv_meny():
     print("5. Beregn areal for rombe")
     print("6. Beregn areal for trapes")
     print("7. Beregn areal for sirkel")
+    print("7. Beregn Omkrets for sirkel")
     print("8. Avslutt")
 
 
 # Funksjon for å beregne arealet av et kvadrat skrives her. Funsjonen skal ta imot et parameter (s)
 # Funksjonen skal returnere arealet
 # Utvikler 1 har ansvaret for å lage denne funksjonen
-def kvadratareal():
-    s_input = float(input("Oppgi side på kvadrat: "))
-
-    kvadratareal_regning = s_input * s_input
-
-    print(f"Svaret er {kvadratareal_regning} cm2")
+def kvadratareal(s):
+    kvadratareal_regning = s * s
+    return(kvadratareal_regning)
 
 # Funksjon for å beregne arealet av et rektangel skrives her. Funsjonen skal ta imot to parameter (g og h)
 # Funksjonen skal returnere arealet
 # Utvikler 1 har ansvaret for å lage denne funksjonen
-def rektangelareal():
-    g_input = float(input("Oppgi lengde på kvadrat: "))
-    h_input = float(input("Oppgi høyde på kvadrat: "))
+def rektangelareal(g,h):
 
-    rektangelareal_regning = g_input * h_input
+    rektangelareal_regning = g * h
+    return(rektangelareal_regning)
 
-    print(f"Svaret er {rektangelareal_regning} cm2")
+    
 
 # Funksjon for å beregne arealet av en trekant skrives her. Funsjonen skal ta imot to parameter (g og h)
 # Funksjonen skal returnere arealet
 # Utvikler 1 har ansvaret for å lage denne funksjonen
-def trekantareal():
-    g_input = float(input("Oppgi lengde på trekant: "))
-    h_input = float(input("Oppgi høyde på trekant: "))
+def trekantareal(g,h):
 
-
-    trekantareal_regning = g_input * h_input
-
-=======
-
-    trekantareal_regning = g_input * h_input
-
-    print(f"Svaret er {trekantareal_regning/2} cm2")
+    trekantareal_regning = g * h
+    return(trekantareal_regning/2)
+    
 
 # Funksjon for å beregne arealet av et parallellogram skrives her. Funsjonen skal ta imot to parameter (g og h)
 # Funksjonen skal returnere arealet
@@ -113,26 +101,36 @@ while ans != "8":
     if ans=="1":
         clear()
         print("\nHer bergnes arealet av et kvadrat")
+        s_input = float(input("Oppgi side på kvadrat: "))
+        print("Svaret er: ", kvadratareal(s_input))
         venter=input("Trykk ENTER for å fortsette!")
-        venter=input("Trykk ENTER for å fortsette!")
+        
     elif ans=="2":
         clear()
         print("\nHer bergnes arealet av et rektangel")
+        g_input = float(input("Oppgi lengde på kvadrat: "))
+        h_input = float(input("Oppgi høyde på kvadrat: "))
+
+        print(f"Svaret er: ", rektangelareal(g_input,h_input))
         venter=input("Trykk ENTER for å fortsette!")
-        rektangelareal()
-        venter=input("Trykk ENTER for å fortsette!")
+
     elif ans=="3":
         clear()
         print("\nHer bergnes arealet av en trekant")
+        
+        g_input = float(input("Oppgi lengde på trekant: "))
+        h_input = float(input("Oppgi høyde på trekant: "))
+
+        print(f"Svaret er: ", trekantareal(g_input,h_input))
         venter=input("Trykk ENTER for å fortsette!")
-        trekantareal()
-        venter=input("Trykk ENTER for å fortsette!")
+
     elif ans=="4":
         clear()
         print("\nHer bergnes arealet av et parallellogram")
         venter=input("Trykk ENTER for å fortsette!")
         parallellogramareal()
         venter=input("Trykk ENTER for å fortsette!")
+        
     elif ans=="5":
         clear()
         print("\nHer bergnes arealet av en rombe")
@@ -140,6 +138,7 @@ while ans != "8":
         venter=input("Trykk ENTER for å fortsette!") 
         rombe_areal()
         venter=input("Trykk ENTER for å fortsette!") 
+
     elif ans=="6":
         clear()
         print("\nHer bergnes arealet av en trapes")
@@ -147,6 +146,7 @@ while ans != "8":
         venter=input("Trykk ENTER for å fortsette!")         
         trapes_areal()
         venter=input("Trykk ENTER for å fortsette!") 
+
     elif ans=="7":
         clear()
         print("\nHer bergnes arealet av en sirkel")
